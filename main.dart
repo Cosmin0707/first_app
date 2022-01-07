7aff939abc9214de851108ae31ce1a55c8f890d6
Cosmin0707 <91553625+Cosmin0707@users.noreply.github.com>
2022-01-07 14:22:47 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,67 +9,60 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     //TODO: implement createState
-
     return _MyAppState();
   }
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
-    {
-      'questionText': 'What\'s your favourite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
-    },
-    {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': ['Rabbit', 'Snake', 'Lion', 'Tiger'],
-    },
-    {
-      'questionText': 'Who\'s your favourite superhero?',
-      'answers': ['Captain America', 'Iron Man', 'Hulk', 'Thor'],
-    },
-  ];
-
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      _questionIndex = _questionIndex + 1;
+      _questionIndex += 1;
     });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    var _questions;
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
-    }
-
-    @override
-    Widget build (BuildContext context) {
-      /*var dummy = const ['Hello'];
-    dummy.add('Max');
-    dummy = [];
-    questions = []; //does not work if questions is a 'const'
-    */
-
-      return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Column(
-            children: [
-              Question(
-                questions[_questionIndex]['questionText'] as String,
-              ),
-              ...(questions[_questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                return Answer(_answerQuestion, answer);
-              }).toList()
-            ],
-          ),
-        ),
-      );
     }
   }
 
   @override
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+  Widget build(BuildContext context) {
+    final _questions = const [
+      {
+        'questionText': 'What\'s your favourite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': ['Cat', 'Dog', 'Whale', 'Tiger'],
+      },
+      {
+        'questionText': 'Who\'s your favourite instructor?',
+        'answers': ['BUBL', 'Cosmin', 'BRS', 'Didi'],
+      },
+    ];
+
+//var dummy = const ['Hello'];
+//dummy.add('Max');
+//print(dummy);
+//dummy = [];
+//questions = []; does not work if this var is const.
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions
+                )
+            : Result(),
+      ),
+    );
+  }
 }
