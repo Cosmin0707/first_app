6ba4ba48a470da9cddcaf1e6725f87ba15caaad3
Cosmin0707 <91553625+Cosmin0707@users.noreply.github.com>
2022-01-10 14:03:01 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,67 +1,92 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
-import './quiz.dart';
-import './result.dart';
+
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    //TODO: implement createState
-    return _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
   }
 }
 
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex += 1;
-    });
-    print(_questionIndex);
-    var _questions;
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
-    }
-  }
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Gym',
+      amount: 49.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Groceries',
+      amount: 16.55,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final _questions = const [
-      {
-        'questionText': 'What\'s your favourite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Cat', 'Dog', 'Whale', 'Tiger'],
-      },
-      {
-        'questionText': 'Who\'s your favourite instructor?',
-        'answers': ['BUBL', 'Cosmin', 'BRS', 'Didi'],
-      },
-    ];
-
-//var dummy = const ['Hello'];
-//dummy.add('Max');
-//print(dummy);
-//dummy = [];
-//questions = []; does not work if this var is const.
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first app'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-                questions: _questions
-                )
-            : Result(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(100),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(children: <Widget>[
+                    Text(tx.title),
+                    Text(
+                      tx.date.toString(),
+                    ),
+                  ],),
+                ],),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
